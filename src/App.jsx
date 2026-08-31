d8749ac8d1804c96a37f1b3846c46b7413d47e19
malvis01 <johnnybecker586@gmail.com>
2026-08-31 17:55:07 +0100

    Create App component for BayLINK application

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>BayLINK</h1>
+      <p>Connecting Bayelsa's businesses, people and opportunities.</p>
+    </main>
+  );
+}
+
+export default App;
