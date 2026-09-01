02b2b7ffa2f518e9cbac08b0d24d58337cea4287
malvis01 <johnnybecker586@gmail.com>
2026-09-01 08:56:35 +0100

    Add protected /admin route for BayLINK dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AdminDashboard from "./AdminDashboard";
 import "./App.css";
+
+function Root() {
+  const isAdminRoute = window.location.pathname.replace(/\/$/, "") === "/admin";
+
+  if (isAdminRoute) {
+    return <AdminDashboard onClose={() => { window.location.href = "/"; }} />;
+  }
+
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
